fd156ac181b62ed96290429e236436e389472062
Kael <jalcantarabedtc@gmail.com>
2024-12-08 03:10:14 +0800

    test

--- blendjoy/asgi.py
+++ blendjoy/asgi.py
@@ -8,18 +8,24 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from app.websockets import routing
-
+# Ensure Django is fully initialized before anything else
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blendjoy.settings")
+django.setup()
+
+from app.websockets import routing  # Import after django.setup()
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        ),
     }
 )
