7af8616c5f3f15539a4a346ef21a13f8de0cb772
Kael <jalcantarabedtc@gmail.com>
2024-12-08 00:26:47 +0800

    Update routing.py

--- app/websockets/routing.py
+++ app/websockets/routing.py
@@ -3,6 +3,6 @@ from .consumer import CollaborationConsumer, ChatConsumer
 
 # the empty string routes to ChatConsumer, which manages the chat functionality.
 websocket_urlpatterns = [
-    re_path(r"ws/collaborate/(?P<slug>[\w-]+)/$", CollaborationConsumer.as_asgi()),
+    path("ws/collaboration/<slug:slug>/", CollaborationConsumer.as_asgi()),
     path("ws/chat/<str:pk>/", ChatConsumer.as_asgi()),
 ]
